ca96a05d9b9c931b9d68e506478593c9e63b065f
Localhost127.0.0.1 <localhost127001@localhost127.0.0.1>
2020-11-15 16:22:29 +0700

    install bootraps

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			todo: []
-		}
-		
-	}
 	
-	getTodo = async () => {
-		await axios({
-			method: 'get',
-			url: 'https://jsonplaceholder.typicode.com/todos/16',
-			responseType: 'json'
-		  }).then(response => {
-			this.setState({
-				todo:response.data
-			})
-		});
-	}
-
-	componentDidMount() {
-		this.getTodo();
-	}
-
 	render() {
-		const { todo } = this.state;
 		return (
 			<div>
-				<p>My Name is {todo.title}</p>
-				<p>Completed is  { todo.completed ? 'true' : 'false' }</p>
+				<p></p>
 			</div>
 		);
 	}
